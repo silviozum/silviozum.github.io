9e75c0ec19a95826055a6d2be255dd46ddd130fe
SIlvi0 <silviozum@hotmail.com>
2015-03-08 17:21:10 -0300

    init project whit 40 front

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "_css/main.css": "_less/main.less"
+        }
+      }
+    },        
+    concat: {
+      css: {
+        src: [
+        '_css/*'
+        ],
+        dest: 'css/main.css'
+      }
+    },
+    cssmin: {
+      css: {
+        src: 'css/main.css',
+        dest: 'css/main.min.css'
+      }
+    },
+    jekyll: {
+      options: {                          
+        src: '.'
+      },
+      dist: {
+        options: {
+          dest: './_site',
+          config: '_config-dev.yml'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: ['_css/*', '_less/*'],
+        tasks: ['less', 'concat:css', 'cssmin:css']
+      },
+      html: {
+        files: ['*.html', '_includes/*.html', '_layouts/*.html', '_posts/*'],
+        tasks: ['jekyll'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: './_site'
+        }
+      }
+    },
+
+  });
+require('load-grunt-tasks')(grunt);
+grunt.registerTask('default', ['less', 'concat:css', 'cssmin:css', 'jekyll']);
+grunt.registerTask('css', ['less', 'concat:css', 'cssmin:css']);
+grunt.registerTask('server', ['connect', 'watch']);
+};
